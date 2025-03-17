1c0a68fc1407cba1f38b23dcf2925d3024ff1b14
Mansi Saraswat <mansisaraswat030@gmail.com>
2025-03-17 15:57:04 +0530

    footer added

---
+++ components/footer/footer.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      © EndScripts 2024. All Rights Reserved.
+    </footer>
+  );
+};
+
+export default Footer;
