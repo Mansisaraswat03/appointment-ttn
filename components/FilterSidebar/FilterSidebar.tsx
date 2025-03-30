3314056f9d9c57f7066c8fb064b539cc37b5cdcf
Mansi Saraswat <mansisaraswat030@gmail.com>
2025-03-31 00:13:52 +0530

    bug fixes

--- components/FilterSidebar/FilterSidebar.tsx
+++ components/FilterSidebar/FilterSidebar.tsx
@@ -1,86 +1,80 @@
 "use client";
-
+import { useSearchParams } from "next/navigation";
 import styles from "./FilterSidebar.module.css";
 
 type FilterSidebarProps = {
-  onFilterChange: (filters: any) => void;
+  onFilterChange: (filterKey: string, filterValue: string, checked: boolean) => void;
+  appliedFilters: Record<string, string | string[]>;
 };
 
-const FilterSidebar = ({ onFilterChange }: FilterSidebarProps) => {
-  const handleFilterChange = () => {
-    onFilterChange({});
+const FilterSidebar = ({ onFilterChange, appliedFilters }: FilterSidebarProps) => {
+  const searchParams = useSearchParams();
+
+  const isChecked = (filterKey: string, filterValue: string) => {
+    const filterValues = searchParams.getAll(filterKey);
+    return filterValues.includes(filterValue);
   };
+
+  const handleCheckboxChange = (filterKey: string, filterValue: string) => {
+    const checked = !isChecked(filterKey, filterValue); 
+    onFilterChange(filterKey, filterValue, checked);
+  };
+  
 
   return (
     <aside className={styles.sidebar}>
       <div className={styles.header}>
-      <h3 className={styles.title}>Filter By:</h3>
-      <button className={styles.resetBtn} onClick={handleFilterChange}>Reset</button>
+        <h3 className={styles.title}>Filter By:</h3>
+        <button className={styles.resetBtn} onClick={() => onFilterChange("", "", false)}>Reset</button>
       </div>
 
       <div className={styles.section}>
         <h4>Rating</h4>
         <ul>
-          <li>
-            <input type="checkbox" /> Show all
-          </li>
-          <li>
-            <input type="checkbox" /> 1 Star
-          </li>
-          <li>
-            <input type="checkbox" /> 2 Star
-          </li>
-          <li>
-            <input type="checkbox" /> 3 Star
-          </li>
-          <li>
-            <input type="checkbox" /> 4 Star
-          </li>
-          <li>
-            <input type="checkbox" /> 5 Star
-          </li>
+          {[1, 2, 3, 4, 5].map((rating) => (
+            <li key={rating}>
+              <input
+                type="checkbox"
+                checked={isChecked("rating", rating.toString())}
+                onChange={() => handleCheckboxChange("rating", rating.toString())}
+              />
+              {rating} Star
+            </li>
+          ))}
         </ul>
       </div>
 
       <div className={styles.section}>
         <h4>Experience</h4>
         <ul>
-          <li>
-            <input type="checkbox" /> 16+ years
-          </li>
-          <li>
-            <input type="checkbox" /> 10-15 years
-          </li>
-          <li>
-            <input type="checkbox" /> 5-10 years
-          </li>
-          <li>
-            <input type="checkbox" /> 3-5 years
-          </li>
-          <li>
-            <input type="checkbox" /> 1-3 years
-          </li>
-          <li>
-            <input type="checkbox" /> 0-1 years
-          </li>
+          {["16", "10-15", "5-10", "3-5", "1-3", "0-1"].map((exp) => (
+            <li key={exp}>
+              <input
+                type="checkbox"
+                checked={isChecked("experience", exp)}
+                onChange={() => handleCheckboxChange("experience", exp)}
+              />
+              {exp} years
+            </li>
+          ))}
         </ul>
       </div>
 
       <div className={styles.section}>
         <h4>Gender</h4>
         <ul>
-          <li>
-            <input type="checkbox" /> Show all
-          </li>
-          <li>
-            <input type="checkbox" /> Male
-          </li>
-          <li>
-            <input type="checkbox" /> Female
-          </li>
+          {["male", "female", "others"].map((gender) => (
+            <li key={gender}>
+              <input
+                type="checkbox"
+                checked={isChecked("gender", gender)}
+                onChange={() => handleCheckboxChange("gender", gender)}
+              />
+              {gender.charAt(0).toUpperCase() + gender.slice(1)}
+            </li>
+          ))}
         </ul>
       </div>
-
     </aside>
   );
 };
--- components/doctors/doctors.tsx
+++ components/doctors/doctors.tsx
@@ -1,34 +1,31 @@
 "use client";
 import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 import DoctorCard from "../DoctorCard/DoctorCard";
 import FilterSidebar from "../FilterSidebar/FilterSidebar";
 import SearchBar from "../SearchBar/SearchBar";
-import styles from "./DoctorPage.module.css";
-import axios from "axios";
-import { toast } from "react-hot-toast";
-import { Doctor } from "@/types/types";
-import { useRouter, useSearchParams } from "next/navigation";
 import DoctorSkeleton from "../skeleton/DoctorSkeleton";
+import styles from "./DoctorPage.module.css";
+import { Doctor } from "@/types/types";
 
 const Doctors = () => {
   const [doctors, setDoctors] = useState<Doctor[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
-  // const [query, setQuery] = useState<string>("");
+
   const searchParams = useSearchParams();
   const router = useRouter();
-  
-  const query = searchParams.get("search") || "";
 
-  const fetchDoctors = async (searchQuery = "") => {
+  const filters = Object.fromEntries(searchParams.entries()); 
+
+  const fetchDoctors = async () => {
     setLoading(true);
     try {
-      const response = await axios.get(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/v1/api/doctors`,
-        {
-          params: searchQuery ? { search: searchQuery } : {},
-        }
-      );
-      setDoctors(response?.data?.data?.doctors);
+      const response = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/v1/api/doctors`, {
+        params: filters,
+      });
+      setDoctors(response?.data?.data?.doctors || []);
     } catch (error) {
       toast.error("Failed to fetch doctors");
     } finally {
@@ -37,8 +34,8 @@ const Doctors = () => {
   };
 
   useEffect(() => {
-    fetchDoctors(query);
-  }, [query]);
+    fetchDoctors();
+  }, [searchParams]);
 
   const handleSearch = (term: string) => {
     const params = new URLSearchParams(searchParams);
@@ -50,8 +47,18 @@ const Doctors = () => {
     router.push(`?${params.toString()}`, { scroll: false });
   };
 
-  const handleFilterChange = (filters: any) => {
-    
+  const handleFilterChange = (filterKey: string, filterValue: string, checked: boolean) => {
+    const params = new URLSearchParams(searchParams);
+
+    if (checked) {
+      params.append(filterKey, filterValue);
+    } else {
+      const values = params.getAll(filterKey).filter(val => val !== filterValue);
+      params.delete(filterKey);
+      values.forEach(val => params.append(filterKey, val));
+    }
+
+    router.push(`?${params.toString()}`, { scroll: false });
   };
 
   return (
@@ -69,7 +76,7 @@ const Doctors = () => {
 
       <div className={styles.contentWrapper}>
         <aside className={styles.sidebar}>
-          <FilterSidebar onFilterChange={handleFilterChange} />
+          <FilterSidebar onFilterChange={handleFilterChange} appliedFilters={filters} />
         </aside>
 
         <section className={styles.doctorSection}>
@@ -81,17 +88,6 @@ const Doctors = () => {
               doctors.map((doctor, index) => (
                 <DoctorCard key={index} {...doctor} />
               ))}
-          </div>
-
-          <div className={styles.paginationWrapper}>
-            <button className={styles.paginationBtn}>&laquo; Prev</button>
-            {Array.from({ length: 3 }).map((_, index) => (
-              <button key={index} className={styles.paginationBtn}>
-                {index + 1}
-              </button>
-            ))}
-            <span className={styles.paginationEllipsis}>...</span>
-            <button className={styles.paginationBtn}>Next &raquo;</button>
           </div>
         </section>
       </div>
